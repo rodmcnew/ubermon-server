a470b20398fb9e4740aa938beb594ffa2223cc63
rodmcnew <rodmcnew@gmail.com>
2015-11-20 15:22:10 -0600

    copy from other repo

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -7,33 +7,32 @@ module.exports = function (grunt) {
                 generated: {
                     files: [
                         {
-                            //dest: '.tmp/concat/js/app.js',
                             dest: 'dist/bower_components.js',
                             src: [
                                 'bower_components_not_bower/Chart.js-skip-xlabels/Chart.min.js',
                                 'bower_components/angular/angular.min.js',
                                 'bower_components/angular-chart.js/dist/angular-chart.min.js',
                                 'bower_components_not_bower/angular/angular-resource.js',
-                                //'bower_components_not_bower/angular/angular-route.js',
                                 'js/lb-services.js'
                             ]
                         }
                     ]
                 }
-            }//,
-            //uglify: {
-            //    generated: {
-            //        files: [
-            //            {
-            //                dest: 'dist/js/app.js',
-            //                src: ['.tmp/concat/js/app.js']
-            //            }
-            //        ]
-            //    }
-            //}
+            },
+            uglify: {
+                generated: {
+                    files: [
+                        {
+                            dest: 'dist/bower_components.min.js',
+                            src: ['dist/bower_components.js']
+                        }
+                    ]
+                }
+            }
         }
     );
 
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.registerTask('default', ['concat']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
