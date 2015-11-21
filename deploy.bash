c0ce8bbe04186f140af4ffcb8cca717a7e0cb0a3
rodmcnew <rodmcnew@gmail.com>
2015-11-21 13:44:43 -0600

    update

--- deploy.bash
+++ deploy.bash
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 # This script deploys the app to Heroku
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd $DIR/
-git remote add heroku https://git.heroku.com/ubermon.git
-git remote add heroku.remote https://git.heroku.com/remote1-ubermon.git
-git push heroku master
-git push heroku.remote master
-
+cd $DIR/../deploy.ubermon
+if [ $? -eq 0 ]; then
+    rm -R ./*
+    cp -R ../ubermon/* ./
+    # Bring main .gitignore so node_modules don't go to Heroku
+    cp ../ubermon/.gitignore ./
+    # Ensure secrets.js makes it to Heroku
+    rm ./secret/.gitignore
+    git add .
+    git commit -m "copy from other repo"
+    git push heroku master
+    git push heroku.remote master
+    cd $DIR
+else
+    echo deploy.ubermon folder does not exist
+fi
