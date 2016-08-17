381a44b5f01c7b2439fa2f1528be60fd819cd2e7
rodmcnew <rodmcnew@gmail.com>
2016-08-17 14:41:42 -0500

    copy from main repo

--- client/app_components/ubermon/contact-us-form/contact-us-form.js
+++ client/app_components/ubermon/contact-us-form/contact-us-form.js
@@ -1,31 +1,31 @@
-angular.module('ubermon').directive('ubermonContactUsForm', function (User, $scope, $location, $http) {
-
-    $scope.messageSent = false;
-    $scope.message = {
-        email: '',
-        body: ''
-    };
-
-    //Send the reset password email
-    $scope.sendMessage = function (message) {
-        //$scope.error = '';
-        //User.resetPassword(
-        //    {email: email},
-        //    function () {
-        //        $scope.resetEmailSent = true;
-        //    },
-        //    handleLBError
-        //)
-        $scope.messageSent = true;
-    };
+console.log(1);
+angular.module('ubermon').directive('ubermonContactUsForm', function () {
 
     function link($scope) {
+        $scope.messageSent = false;
+        $scope.message = {
+            email: '',
+            body: ''
+        };
+
+        //Send the reset password email
+        $scope.sendMessage = function (message) {
+            //$scope.error = '';
+            //User.resetPassword(
+            //    {email: email},
+            //    function () {
+            //        $scope.resetEmailSent = true;
+            //    },
+            //    handleLBError
+            //)
+            $scope.messageSent = true;
+        };
     }
 
     // Return the directive configuration
     return {
         link: link,
         restrict: 'E',
-        templateUrl: '/components/ubermon/contact-us-form/contact-us-form.html'
+        templateUrl: '/app_components/ubermon/contact-us-form/contact-us-form.html'
     }
 });
