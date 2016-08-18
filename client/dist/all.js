a6929a2f3c16ca0499d9214f41916e0edeffdf96
rodmcnew <rodmcnew@gmail.com>
2016-08-18 00:20:28 -0500

    copy from main repo

--- client/dist/all.js
+++ client/dist/all.js
@@ -3459,28 +3459,26 @@ angular.module(
     ]
 );
 
-var ubermonConfig = {
-    monitorTypes: {
-        'h': 'HTTP(s)',
-        'p': 'Ping',
-        'o': 'Port',
-        'k': 'Keyword'
-    },
-    monitorIntervals: {
-        1: 'Every minute',
-        2: 'Every 2 minutes',
-        5: 'Every 5 minutes',
-        10: 'Every 10 minutes',
-        15: 'Every 15 minutes',
-        20: 'Every 20 minutes',
-        30: 'Every 30 minutes',
-        60: 'Every 60 minutes'
-    },
-    recaptchaPubKey: '6LcCeRMTAAAAAJOmu2kbjXyOs07yf28tFt2sn9bF'
-};
-
 angular.module('ubermon').factory('ubermonConfig', function () {
-    return ubermonConfig;
+    return {
+        monitorTypes: {
+            'h': 'HTTP(s)',
+            'p': 'Ping',
+            'o': 'Port',
+            'k': 'Keyword'
+        },
+        monitorIntervals: {
+            1: 'Every minute',
+            2: 'Every 2 minutes',
+            5: 'Every 5 minutes',
+            10: 'Every 10 minutes',
+            15: 'Every 15 minutes',
+            20: 'Every 20 minutes',
+            30: 'Every 30 minutes',
+            60: 'Every 60 minutes'
+        },
+        recaptchaPubKey: '6LcCeRMTAAAAAJOmu2kbjXyOs07yf28tFt2sn9bF'
+    };
 });
 
 angular.module('ubermon').directive('ubermonContactEdit', function () {
@@ -3781,7 +3779,7 @@ angular.module('ubermon').directive('ubermonDashboard', function (Monitor, Conta
 /**
  * @TODO move the "create user" and "login" forms to directives
  */
-angular.module('ubermon').controller('ubermonHome', function (User, Contact, $scope, $window, vcRecaptchaService) {
+angular.module('ubermon').controller('ubermonHome', function (User, Contact, $scope, $window, vcRecaptchaService, ubermonConfig) {
 
     $scope.newUser = {};
     $scope.loginUser = {};
