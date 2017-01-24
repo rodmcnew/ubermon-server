0e8ed3013632372be9277369b3f41af32a89da6c
rodmcnew <rodmcnew@gmail.com>
2017-01-24 13:26:13 -0600

    add build files

--- deploy.bash
+++ deploy.bash
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 # This script deploys the app to Heroku
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd $DIR/../deploy.ubermon
-if [ $? -eq 0 ]; then
-    rm -R ./*
-    cp -R ../ubermon-uptime-monitor/* ./
-    # Bring main .gitignore so node_modules don't go to Heroku
-    cp ../ubermon-uptime-monitor/.gitignore ./
-    git add .
-    git commit -m "copy from main repo"
-    git push heroku master
-    #git push heroku.remote master
-    cd $DIR
-else
-    echo deploy.ubermon folder does not exist
-fi
+git remote add production https://git.heroku.com/ubermon.git
+git remote add productionremote https://git.heroku.com/remote1-ubermon.git
+git add . && git commit -m "add build files"
+git push origin master
+git push production master
+#git push productionremote master
